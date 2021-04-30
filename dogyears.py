b25e6bb387bd37682ef2d8f084224bbbb2b56c22
Jaime Rogstad <66330786+jrog20@users.noreply.github.com>
2021-04-30 08:57:24 -0400

    Update dogyears.py

--- dogyears.py
+++ dogyears.py
@@ -7,7 +7,7 @@ DOG_YEARS = 7
 def main():
   human_age = int(input('Enter human age: '))
   dog_age = convert_to_dog(human_age)
-  print('Dog age is: ' + str(dog_age))
+  print('Dog age is:', dog_age)
 
 def convert_to_dog(human_age):
   return human_age * DOG_YEARS
